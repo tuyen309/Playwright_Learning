be5451f517a035af9f9b832047c6ffa25b6c838e
Tuyen Nguyen <tuyengghf18@gmail.com>
2025-08-25 11:23:41 +0700

    run_1

--- test_practice/demo.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Check Title Page', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await expect(page).toHaveTitle(/Swag Labs/);
-});
---
+++ test_practice/todo.spec.ts
@@ -0,0 +1,27 @@
+const { test } = require('@playwright/test');
+const { TodoPage } = require('./test_pracetice/todo-page'); // Adjust the import path as necessary
+
+test.describe('todo tests', () => {
+  let todoPage;
+
+  test.beforeEach(async ({ page }) => {
+    todoPage = new TodoPage(page);
+    await todoPage.goto();
+    await todoPage.addToDo('item1');
+    await todoPage.addToDo('item2');
+  });
+
+  test.afterEach(async () => {
+    await todoPage.removeAll();
+  });
+
+  test('should add an item', async () => {
+    await todoPage.addToDo('my item');
+    // ...
+  });
+
+  test('should remove an item', async () => {
+    await todoPage.remove('item1');
+    // ...
+  });
+});
